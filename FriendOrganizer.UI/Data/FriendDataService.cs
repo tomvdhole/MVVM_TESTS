f1b8b904cde0708fe12fe87fe290292adcef2926
Tom <tomvandenhole@gmail.com>
2019-03-03 15:40:14 +0100

    Init

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,28 @@
+﻿using FriendOrganizer.DataAccess;
+using FriendOrganizer.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        private Func<FriendOrganizerDbContext> _contextCreator;
+
+        public FriendDataService(Func<FriendOrganizerDbContext> contextCreator)
+        {
+            _contextCreator = contextCreator;
+        }
+
+        public async Task<List<Friend>> GetAllAsync()
+        {
+            using (var ctx = _contextCreator())
+            {
+                return await ctx.Friends.AsNoTracking().ToListAsync();
+            }
+        }
+    }
+}
---
+++ FriendOrganizer.UI/StartUp/Bootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using FriendOrganizer.DataAccess;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.ViewModel;
+
+namespace FriendOrganizer.UI.StartUp
+{
+    public class Bootstrapper
+    { 
+        public IContainer BootStrap()
+        {
+
+            var builder = new ContainerBuilder();
+            builder.RegisterType<FriendOrganizerDbContext>().AsSelf();
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<FriendDataService>().As<IFriendDataService>();
+
+            return builder.Build();
+        }
+    }
+}
